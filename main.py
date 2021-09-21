a07acdc2dfa9e013d7cd0ca23152ea0c0f0e3669
Dillon Erb <dillon@paperspace.com>
2021-09-21 00:30:29 -0400

    static adoro-web

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from demo import 
 
 app = FastAPI()
 
-
-@app.get("/question")
-def question():
-    return {"answer": 42}
-
+app.mount("/", StaticFiles(directory="adoro-web"), name="adoro-web")
 
 @app.post("/generate")
 def create_adoro(reference_png_base64: str):
