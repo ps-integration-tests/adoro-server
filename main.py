af3ea3f87ac4c3cdadd8daa90f258fb456915d10
Dillon Erb <dillon@paperspace.com>
2021-09-21 16:24:34 -0400

    route order

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="static", html=True),  name="adoro-web-static")
-
 @app.post("/generate")
 def create_adoro(reference_png_base64: str):
     pass
+
+app.mount("/", StaticFiles(directory="static", html=True),  name="adoro-web-static")
