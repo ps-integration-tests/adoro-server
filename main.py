777c114efcf098e109107cffe4287da7602406ee
Dillon Erb <dillon@paperspace.com>
2021-09-21 00:47:07 -0400

    lint

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from demo import 
-
 app = FastAPI()
 
 app.mount("/", StaticFiles(directory="adoro-web"), name="adoro-web")
